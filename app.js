cf8c6e9d505dad4b50347dfaf0654dac3bf34792
barinoffarseni <129789891+barinoffarseni@users.noreply.github.com>
2024-06-30 15:18:37 +0300

    Update app.js

--- app.js
+++ app.js
@@ -4,37 +4,55 @@ ctx = canvas.getContext('2d')
 canvas.width = 1024
 canvas.height = 576
 
-ctx.fillStyle = "black";
-ctx.fillRect(0, 0, canvas.width, canvas.height)
+// ctx.fillStyle = "black";
+// ctx.fillRect(0, 0, canvas.width, canvas.height)
 
 class Sprite {
-    constructor(position) {
+    constructor({ position, velocity }) {
         this.position = position
+        this.velocity = velocity
     }
 
     draw() {
         ctx.fillStyle = 'red'
         ctx.fillRect(this.position.x, this.position.y, 50, 150)
     }
+
+    update() {
+        this.draw()
+        this.position.x += this.velocity.x
+        this.position.y += this.velocity.y
+    }
 }
 
 const player = new Sprite({
-    x: 0,
-    y: 0
+    position: {
+        x: 0,
+        y: 0
+    },
+    velocity: {
+        x: 0,
+        y: 10
+    }
 })
-
-player.draw()
 
 const enemy = new Sprite({
-    x: canvas.width / 2,
-    y: 0
+    position: {
+        x: canvas.width / 2,
+        y: 0
+    },
+    velocity: {
+        x: 0,
+        y: 1
+    }
 })
-
-enemy.draw()
 
 function animate() {
     window.requestAnimationFrame(animate)
-    console.log('аниме хуйня')
+    ctx.fillStyle = "black";
+    ctx.fillRect(0, 0, canvas.width, canvas.height)
+    player.update()
+    enemy.update()
 }
 
 animate()

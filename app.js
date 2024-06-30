4e51672dcb93af4ae7a875410371a82931e9fadf
barinoffarseni <129789891+barinoffarseni@users.noreply.github.com>
2024-06-30 15:52:36 +0300

    Added player and enemy (#2)

--- app.js
+++ app.js
@@ -1,14 +1,64 @@
-canvas = document.querySelector('canvas')
-ctx = canvas.getContext('2d')
+const canvas = document.querySelector('canvas')
+const ctx = canvas.getContext('2d')
 
 canvas.width = 1024
 canvas.height = 576
 
-ctx.fillStyle = "red";
-ctx.fillRect(0, 0, canvas.width, canvas.height)
-ctx.fillStyle = "green";
-ctx.fillRect(canvas.width / 2, 0, canvas.width, canvas.height)
-ctx.fillStyle = "blue";
-ctx.fillRect(0, canvas.height / 2, canvas.width, canvas.height)
-ctx.fillStyle = "yellow";
-ctx.fillRect(canvas.width / 2, canvas.height / 2, canvas.width, canvas.height)
+const gravity = 0.2
+
+class Sprite {
+    constructor({ position, velocity }) {
+        this.position = position
+        this.velocity = velocity
+        this.height = 150
+    }
+
+    draw() {
+        ctx.fillStyle = 'red'
+        ctx.fillRect(this.position.x, this.position.y, 50, this.height)
+    }
+
+    update() {
+        this.draw()
+        this.position.x += this.velocity.x
+        this.position.y += this.velocity.y
+
+        if (this.position.y + this.height >= canvas.height) {
+            this.velocity.y = 0
+        } else {
+            this.velocity.y += gravity
+        }
+    }
+}
+
+const player = new Sprite({
+    position: {
+        x: 0,
+        y: 0
+    },
+    velocity: {
+        x: 0,
+        y: 0
+    }
+})
+
+const enemy = new Sprite({
+    position: {
+        x: canvas.width / 2,
+        y: 0
+    },
+    velocity: {
+        x: 0,
+        y: 0
+    }
+})
+
+function animate() {
+    window.requestAnimationFrame(animate)
+    ctx.fillStyle = "black";
+    ctx.fillRect(0, 0, canvas.width, canvas.height)
+    player.update()
+    enemy.update()
+}
+
+animate()

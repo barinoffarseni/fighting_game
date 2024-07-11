1c75d7488d914fe2e131657bc1fad1f608418e53
barinoffarseni <129789891+barinoffarseni@users.noreply.github.com>
2024-07-11 12:56:55 +0300

    Feature movement (#3)

--- app.js
+++ app.js
@@ -6,6 +6,17 @@ canvas.height = 576
 
 const gravity = 0.2
 
+const keys = {
+    w: false,
+    a: false,
+    s: false,
+    d: false,
+    up: false,
+    left: false,
+    down: false,
+    right: false
+}
+
 class Sprite {
     constructor({ position, velocity }) {
         this.position = position
@@ -57,8 +68,81 @@ function animate() {
     window.requestAnimationFrame(animate)
     ctx.fillStyle = "black";
     ctx.fillRect(0, 0, canvas.width, canvas.height)
+    control()
     player.update()
     enemy.update()
 }
 
 animate()
+
+function control() {
+    player.velocity.x = 0
+    if (keys.w) {
+        player.velocity.y = -10
+    }
+    if (keys.d) {
+        player.velocity.x = 1
+    }
+    if (keys.a) {
+        player.velocity.x = -1
+    } 
+
+    enemy.velocity.x = 0
+    if (keys.up) {
+        enemy.velocity.y = -10
+    }
+    if (keys.right) {
+        enemy.velocity.x = 1
+    }
+    if (keys.left) {
+        enemy.velocity.x = -1
+    }
+}
+
+window.addEventListener('keydown', keydown)
+function keydown(event) {
+    switch (event.key) {
+        case 'd':
+            keys.d = true
+            break
+        case 'a':
+            keys.a = true
+            break
+        case 'w':
+            keys.w = true
+            break
+        case 'ArrowRight':
+            keys.right = true
+            break
+        case 'ArrowLeft':
+            keys.left = true
+            break
+        case 'ArrowUp':
+            keys.up = true
+            break
+    }  
+}
+
+window.addEventListener('keyup', keyup)
+function keyup(event) {
+    switch (event.key) {
+        case 'd':
+            keys.d = false
+            break
+        case 'a':
+            keys.a = false
+            break
+        case 'w':
+            keys.w = false
+            break
+        case 'ArrowRight':
+            keys.right = false
+            break
+        case 'ArrowLeft':
+            keys.left = false
+            break
+        case 'ArrowUp':
+            keys.up = false
+            break            
+    }  
+}

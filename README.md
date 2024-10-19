cb481483f1df3126c1f36a19d4585f89c030c669
Ivan Yashin <newparadigma1@gmail.com>
2024-10-19 22:37:47 +0700

    server

--- README.md
+++ README.md
@@ -1,8 +1,15 @@
 # HTML canvas fighting game
 
-tutorial by chriscourses
+[ссылка на игру](https://barinoffarseni.github.io/fighting_game)
+
+## Инструкция по запуску
+
+1. В папке проекта выполнить команду docker-compose up.
+2. Открыть в браузере [http://localhost](http://localhost)
+
+## Благодарности
+
+Игра сделана на основе урока от `Chris Courses` на `youtube`.
 
 * [github](https://github.com/chriscourses/fighting-game)
 * [youtube](https://www.youtube.com/watch?v=vyqbNFMDRGQ)
-
-[link](https://barinoffarseni.github.io/fighting_game)
---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+app.use(express.static('./'))
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+
+  io.emit('event-name', 'Привет браузеру от сервера!');
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+
+  socket.on('event-name', (msg) => {
+    console.log('message: ' + msg);
+  });
+});
+
+server.listen(3000, () => {
+  console.log('listening on *:3000');
+});

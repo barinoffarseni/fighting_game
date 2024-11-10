53c93481ab88d246e300cd62effa1c2f61893d48
barinoffarseni <129789891+barinoffarseni@users.noreply.github.com>
2024-11-10 19:35:54 +0300

    сделал наконец та ки норм удаление из списка

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const { Server } = require("socket.io");
 const io = new Server(server);
 
 const users = []
-let numberOfUsers = 0
 
 app.use(express.static('./'))
 
@@ -18,19 +17,39 @@ io.on('connection', (socket) => {
   const id = socket.handshake.issued
   console.log(id + ' user connected'); // оставь не трогай
 
-  numberOfUsers += 1
-  users.push({number: numberOfUsers, id: id}) 
+  users.push(id) 
   
   io.emit('set-id', id);
   
   console.log(users);
   io.emit('send-user-ids', users);
 
+  socket.on('send-status', (playerId) => {
+    console.log('Получены данные от вкладки:', playerId);
+
+    // Отправляем данные всем остальным вкладкам, кроме отправителя
+    socket.broadcast.emit('get-status', playerId);
+  });
+
+  socket.on('send-status', (enemyId) => {
+    console.log('Получены данные от вкладки:', enemyId);
+
+    // Отправляем данные всем остальным вкладкам, кроме отправителя
+    socket.broadcast.emit('get-status', enemyId);
+  });
+
   // io.emit('event-name', 'Привет браузеру от сервера!');
 
   socket.on('disconnect', () => {
-    console.log(id)
-    delete users[users.findIndex(user => user.id == id)]
+
+    console.log("dddaddadadaadadddddddddddddddd")
+    console.log(users)
+    const index = users.indexOf(id);
+    console.log(index)
+    if (index > -1) { // only splice array when item is found
+      users.splice(index, 1); // 2nd parameter means remove one item only
+    }
+    console.log(users)
     console.log(id + ' user disconnected');// оставь не трогай
   });
 

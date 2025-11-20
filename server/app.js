8634abd86a1ce269449ca61a42e39a87e6f0b8c4
barinoffarseni <129789891+barinoffarseni@users.noreply.github.com>
2025-11-20 20:13:08 +0300

    бомба

--- server/app.js
+++ server/app.js
@@ -9,41 +9,36 @@ const io = require("socket.io")(httpServer, {
   }
 });
 const Timer = require('./timer.js').Timer;
+const Fighter = require('./fighter.js').Fighter;
 
-class Fighter {
-  constructor({ position, velocity }) {
-    this.health = 100
-    this.width = 50
-    this.height = 150
-    this.position = position
-    this.velocity = velocity
-    this.canJump = false
-  }
+// class Fighter {
+//   constructor({ position, velocity }) {
+//     this.health = 100
+//     this.width = 50
+//     this.height = 150
+//     this.position = position
+//     this.velocity = velocity
+//     this.canJump = false
+//   }
 
-  // +y это вниз
-  // -y это вверх
+//   update() {
+//     this.position.x += this.velocity.x
+//     this.position.y += this.velocity.y
 
-  update() {
-    this.position.x += this.velocity.x
-    this.position.y += this.velocity.y
-
-    // если пользователь коснулся пола - возращаем возможность прыгать
-    if (this.position.y + this.height >= 576 - 96) {
-      this.velocity.y = 0
-      this.position.y = 576 - 96 - this.height
-      this.canJump = true
-      // если пользователь НЕ на полу, то прибавляем ускорение вниз и убираем возможность прыгать
-    } else {
-      this.velocity.y += gravity
-      this.canJump = false
-    }
-    this.velocity.x = 0
-  }
-}
+//     if (this.position.y + this.height >= 576 - 96) {
+//       this.velocity.y = 0
+//       this.position.y = 576 - 96 - this.height
+//       this.canJump = true
+//     } else {
+//       this.velocity.y += gravity
+//       this.canJump = false
+//     }
+//     this.velocity.x = 0
+//   }
+// }
 
 const users = []
 const gameObjects = [];
-const gravity = 0.2
 let gameOver = false
 let winner = ''
 let gameTimer = null

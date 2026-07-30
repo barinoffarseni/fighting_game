c3d62390a68d210d70cc34cee8043eef873aae59
barinoffarseni <barinoffarsenii@gmail.com>
2026-07-30 16:53:47 +0300

    wait timer

--- client/app.js
+++ client/app.js
@@ -1,8 +1,6 @@
 const canvas = document.querySelector('canvas')
 const ctx = canvas.getContext('2d')
 
-ctx.lineWidth = 2
-
 canvas.width = 1024
 canvas.height = 576
 
--- client/classes.js
+++ client/classes.js
@@ -323,6 +323,7 @@ class WaitTimer {
     if (gameState === 'stop') {
       ctx.font = this.text.style
       ctx.textAlign = 'center'
+      ctx.lineWidth = 2
       ctx.fillStyle = 'red'
       ctx.fillText(this.timeRemaining, this.text.position.x, this.text.position.y + this.text.offset.y)
       ctx.strokeText(this.timeRemaining, this.text.position.x, this.text.position.y + this.text.offset.y)

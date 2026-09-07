5e0058e4795b0deb0453c5adc73d6dff30743fb0
barinoffarseni <barinoffarsenii@gmail.com>
2026-09-07 10:15:08 +0300

    исправил баг

--- client/app.js
+++ client/app.js
@@ -371,7 +371,7 @@ function setFighter (player, leftHealthBarData, rightHealthBarData) {
     if (!player[player.type]) {
       if (player.type == 'samurai' && samuraiData.position) {
         player.samurai = new Fighter(samuraiData)
-        gameObjects.splice(1, 0, player.samurai)
+        gameObjects.splice(2, 0, player.samurai)
 
         leftHealthBarData.entity = player.samurai
         gameObjects.push(new HealthBar(leftHealthBarData))
@@ -387,7 +387,7 @@ function setFighter (player, leftHealthBarData, rightHealthBarData) {
       }
       if (player.type == 'ninja' && ninjaData.position) {
         player.ninja = new Fighter(ninjaData)
-        gameObjects.splice(1, 0, player.ninja)
+        gameObjects.splice(2, 0, player.ninja)
 
         rightHealthBarData.entity = player.ninja
         gameObjects.push(new HealthBar(rightHealthBarData))
